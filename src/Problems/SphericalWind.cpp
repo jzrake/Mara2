d60f8e0b4cf517c1043ac033fcb5afd8b8ab09bf
Jonathan Zrake <jonathan.zrake@gmail.com>
2018-09-20 11:09:08 -0400

    Write jet launching boundary condition

--- src/Problems/SphericalWind.cpp
+++ src/Problems/SphericalWind.cpp
@@ -27,28 +27,66 @@ using namespace Cow;
 
 
 
-/**
-Boundary conditions for a vertical atmosphere. The x and y directions are
-periodic. The floor is reflecting and the ceiling is currently set to the
-value of the initial condition.
-*/
-class SphericalWindBoundaryCondition : public BoundaryCondition
+// ============================================================================
+class JetLaunchingBoundaryCondition : public BoundaryCondition
 {
 public:
-    SphericalWindBoundaryCondition()
+    static double structureExponent;
+    static double jetOpeningAngle;
+    static double jetGamma;
+    static double jetEnthalpy;
+
+    void setConservationLaw (std::shared_ptr<ConservationLaw> cl) override
     {
-        reflecting.setConservationLaw (std::make_shared<RelativisticMHD>());
+        reflecting.setConservationLaw (cl);
+    }
+
+    void setMeshGeometry (std::shared_ptr<MeshGeometry> mg) override
+    {
+        meshGeometry = mg;
     }
 
     void apply (Cow::Array& A, MeshLocation location, MeshBoundary boundary, int axis, int numGuard) const override
     {
         if (axis == 1)
+        {
             return reflecting.apply (A, location, boundary, axis, numGuard);
-
+        }
         if (boundary == MeshBoundary::left)
-            return reflecting.apply (A, location, boundary, axis, numGuard);
-
+        {
+            return applyInflowAtInnerBoundary (A, numGuard);
+        }
         return outflow.apply (A, location, boundary, axis, numGuard);
+    }
+
+    void applyInflowAtInnerBoundary (Cow::Array& P, int numGuard) const
+    {
+        for (int i = 0; i < numGuard; ++i)
+        {
+            for (int j = numGuard; j < P.size(1) - numGuard; ++j)
+            {
+                const double q = meshGeometry->coordinateAtIndex (i - numGuard, j - numGuard, 0)[1];
+
+                const double alpha  = structureExponent;
+                const double qjet   = jetOpeningAngle;
+                const double w0     = jetEnthalpy;
+                const double fjet   = std::exp (-std::pow (q / qjet, alpha));
+                const double gamma0 = 1.0 + jetGamma * fjet;
+
+                const double vr = std::sqrt (1.0 - 1.0 / (gamma0 * gamma0));
+                const double rho = 1.0;
+                const double pre = rho * w0 / 4;
+
+                P(i, j, 0, 0) = rho;
+                P(i, j, 0, 1) = vr;
+                P(i, j, 0, 2) = 0.0;
+                P(i, j, 0, 3) = 0.0;
+                P(i, j, 0, 4) = pre;
+                P(i, j, 0, 5) = 0.0;
+                P(i, j, 0, 6) = 0.0;
+                P(i, j, 0, 7) = 0.0;
+            }
+        }
     }
 
     bool isAxisPeriodic (int axis) override
@@ -61,9 +99,15 @@ public:
             default: throw std::logic_error ("SphericalWind");
         }
     }
+    std::shared_ptr<MeshGeometry> meshGeometry;
     OutflowBoundaryCondition outflow;
     ReflectingBoundaryCondition reflecting;
 };
+
+double JetLaunchingBoundaryCondition::structureExponent = 1.5;
+double JetLaunchingBoundaryCondition::jetOpeningAngle   = 0.2;
+double JetLaunchingBoundaryCondition::jetGamma          = 5.0;
+double JetLaunchingBoundaryCondition::jetEnthalpy       = 5.0;
 
 
 
@@ -90,6 +134,10 @@ int SphericalWind::run (int argc, const char* argv[])
     user["dind"]    = 3.0;
     user["u0"]      = 1e-1;
     user["peng"]    = 20;
+    user["alpha"]   = 1.5;
+    user["theta0"]  = 0.2;
+    user["gamma0"]  = 5.0;
+    user["w0"]      = 5.0;
 
 
 
@@ -126,17 +174,12 @@ int SphericalWind::run (int argc, const char* argv[])
 
     auto initialData = [&] (double r, double q, double p) -> std::vector<double>
     {
-        double qtor = user["qtor"];
-        double dtor = user["dtor"];
-        double peng = user["peng"];
-
-        double rhor = std::pow (r, -double (user["dind"]));
-        double rhoq = rhor * (qtor == 0.0 ? 1.0 : std::exp (-r) * (1 + dtor * std::exp (-std::pow ((q - M_PI_2) / qtor, 2))));
-        double pre = std::pow (rhor, 4. / 3) * 1e-3 + peng * std::exp (-r);
-        double ur = r * double (user["u0"]);
-        double vr = ur / std::sqrt (1 + ur * ur);
-
-        return std::vector<double> {rhoq, vr, 0, 0, pre, 0, 0, 0};
+        double dind = user["dind"];
+        double rho  = std::pow (r, -dind);
+        double pre  = std::pow (rho, 4. / 3) * 1e-3;
+        double ur   = r * double (user["u0"]);
+        double vr   = ur / std::sqrt (1 + ur * ur);
+        return std::vector<double> {rho, vr, 0, 0, pre, 0, 0, 0};
     };
 
     auto clineUser = Variant::fromCommandLine (argc, argv);
@@ -149,19 +192,24 @@ int SphericalWind::run (int argc, const char* argv[])
     Variant::update (user, chkptUser);
     Variant::update (user, clineUser);
 
+    JetLaunchingBoundaryCondition::structureExponent = user["alpha"];
+    JetLaunchingBoundaryCondition::jetOpeningAngle   = user["theta0"];
+    JetLaunchingBoundaryCondition::jetGamma          = user["gamma0"];
+    JetLaunchingBoundaryCondition::jetEnthalpy       = user["w0"];
+
     auto logger    = std::make_shared<Logger>();
     auto writer    = std::make_shared<CheckpointWriter>();
     auto tseries   = std::make_shared<TimeSeriesManager>();
     auto scheduler = std::make_shared<TaskScheduler>();
+
     double timestepSize = 0.0;
 
     auto bd = std::shared_ptr<BlockDecomposition>();
-    auto bc = std::shared_ptr<BoundaryCondition> (new SphericalWindBoundaryCondition);
+    auto mg = std::shared_ptr<MeshGeometry> (new SphericalMeshGeometry);
     auto rs = std::shared_ptr<RiemannSolver> (new HlleRiemannSolver);
     auto fs = std::shared_ptr<IntercellFluxScheme> (new MethodOfLinesPlm);
-    auto mg = std::shared_ptr<MeshGeometry> (new SphericalMeshGeometry);
+    auto bc = std::shared_ptr<BoundaryCondition>(new JetLaunchingBoundaryCondition);
     auto cl = std::make_shared<RelativisticMHD>();
-
 
     // Set up grid shape.
     // ------------------------------------------------------------------------
@@ -184,6 +232,9 @@ int SphericalWind::run (int argc, const char* argv[])
         mg = bd->decompose();
         bc = bd->createBoundaryCondition (bc);
     }
+    bc->setMeshGeometry (mg);
+    bc->setConservationLaw (cl);
+
     tseries->setLogger (logger);
     scheduler->setLogger (logger);
 
@@ -257,3 +308,23 @@ int SphericalWind::run (int argc, const char* argv[])
     logger->log() << std::endl << user << std::endl;
     return maraMainLoop (status, timestep, condition, advance, *scheduler, *logger);  
 }
+
+
+
+
+
+
+// Initial conditions function for torus-cloud:
+
+// auto initialData = [&] (double r, double q, double p) -> std::vector<double>
+// {
+//     double qtor = user["qtor"];
+//     double dtor = user["dtor"];
+//     double peng = user["peng"];
+//     double rhor = std::pow (r, -double (user["dind"]));
+//     double rhoq = rhor * (qtor == 0.0 ? 1.0 : std::exp (-r) * (1 + dtor * std::exp (-std::pow ((q - M_PI_2) / qtor, 2))));
+//     double pre = std::pow (rhor, 4. / 3) * 1e-3 + peng * std::exp (-r);
+//     double ur = r * double (user["u0"]);
+//     double vr = ur / std::sqrt (1 + ur * ur);
+//     return std::vector<double> {rhoq, vr, 0, 0, pre, 0, 0, 0};
+// };
